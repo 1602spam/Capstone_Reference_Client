6214b66a113a5ae44b5de75c4e6de9695fd81a20
poro912 <40479447+poro912@users.noreply.github.com>
2022-11-24 19:41:59 +0900

    add submodule

---
+++ ClientModule/ClientModule/Client/ClientSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSystem
+{
+	public class ClientSystem
+	{
+		public ClientSystem()
+		{
+			Console.WriteLine("MyMate Clinet Start");
+
+
+		}
+	}
+}
